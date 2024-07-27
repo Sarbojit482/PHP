c5aa10f00f5b829232fd351b00ac819bd354339d
Sarbojit <145925398+Sarbojit482@users.noreply.github.com>
2024-07-27 22:21:18 +0530

    FibonaciSeries.php

--- FibonaciSeries.php
+++ FibonaciSeries.php
@@ -1,3 +1,4 @@
+Method 1:
 <?php
 $n = 20; // Number of Fibonacci numbers to generate
 $fib = [0, 1]; // Starting values of the Fibonacci series
@@ -9,7 +10,9 @@ for ($i = 2; $i < $n; $i++) {
 echo "The first 20 numbers of the Fibonacci series are: ";
 echo implode(", ", $fib);
 ?>
+//method 1 wii give the the sequence
 
+method 2
 <?php
 function fibonacci($n) {
     if ($n == 0) {
@@ -17,11 +20,12 @@ function fibonacci($n) {
     } elseif ($n == 1) {
         return 2;
     } else {
-        return fibonacci($n - 1) + fibonacci($n - 2);
+        return fibonacci($n - 1) + fibonacci($n - 2);// Calculate and add the next Fibonacci number
     }
 }
 
-$n = 21;
+$n = 21;// Number of Fibonacci numbers to generate
 $result = fibonacci($n);
 echo $result;
 ?>
+//Method 2 will give the total of all numbers
